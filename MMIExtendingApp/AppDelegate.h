7760c29c3bbceed27e8ecd4959371143065f11af
Alberto Lagos Toro <alberto.lagos@modmed.com>
2017-10-16 14:50:57 -0300

    Initial Commit

---
+++ MMIExtendingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMIExtendingApp
+//
+//  Created by Alberto Lagos on 10/16/17.
+//  Copyright © 2017 Alberto Lagos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
